2ddca8b248a444e145c65ef680532f7dc1b529c6
Emanuele Simula <simulaemanuele@gmail.com>
2022-05-19 15:43:49 +0200

    exercise: ex. 5 done

---
+++ Esercizio - 5/README.md
@@ -0,0 +1,9 @@
+# Conditionals & Loops - Exercise 5
+Implementare, tramite il blocco `switch-case`, la funzione `calculateSalary`, la quale, dato in input un ruolo, calcola il salario corrispondente.
+Per ogni ruolo il salario percepito è il seguente:
+
+* ceo => 2200€
+* manager => 1800€
+* cto => 1800€
+* developer => 1500€
+* default => 1000€
---
+++ Esercizio - 5/exercise-5.js
@@ -0,0 +1,35 @@
+// ceo => 2200
+// manager => 1800
+// cto => 1800
+// developer => 1500
+// default => 1000
+function calculateSalary(role) {
+  switch(role){
+    case 'ceo' :
+      return '2200';
+    
+    case 'manager':
+      return '1800';
+    
+    case 'cto':
+      return '1800';
+
+    case 'developer':
+      return '1500';
+
+    case 'other':
+      return '1000';
+  }
+}
+
+const ceoSalary = calculateSalary('ceo');
+const managerSalary = calculateSalary('manager');
+const ctoSalary = calculateSalary('cto');
+const developerSalary = calculateSalary('developer');
+const otherSalary = calculateSalary('other');
+
+console.log(ceoSalary);
+console.log(managerSalary);
+console.log(ctoSalary);
+console.log(developerSalary);
+console.log(otherSalary);
