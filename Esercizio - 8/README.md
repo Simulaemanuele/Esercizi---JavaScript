4b37195cf0dcf817d7d8026eb9ee433a7c053733
Emanuele Simula <simulaemanuele@gmail.com>
2022-05-20 16:06:10 +0200

    exercise: ex. 8 done

---
+++ Esercizio - 8/README.md
@@ -0,0 +1,2 @@
+# Conditionals & Loops - Exercise 5
+Implementare, tramite blocchi condizionali e cicli, la funzione `calculateAverageAge`, la quale, dato un array di persone, calcola l'età media
---
+++ Esercizio - 8/exercise-8.js
@@ -0,0 +1,26 @@
+function calculateAverageAge(persons) {
+  let sum = 0;
+
+  for(let i = 0; i < persons.length; i++){
+    sum = sum + persons[i].age;
+  }
+
+  return sum / persons.length;
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const averageAge = calculateAverageAge(persons);
+console.log(persons);
+console.log(`Avarage age: ${averageAge}`);
