66ea01db30606bed9a24dc4b76472f7a31e08d54
Emanuele Simula <simulaemanuele@gmail.com>
2022-05-23 11:49:32 +0200

    exercise: ex. 16 done

---
+++ Esercizio - 16/README.md
@@ -0,0 +1,2 @@
+# Closure - Exercise 3
+Implementare la funzione `createStore` che crea un array vuoto di prodotti e restituisce una closure che si occupa di aggiungere prodotti allo store. Per ogni prodotto aggiunto allo store eseguire il `console.log` dell'intero store
---
+++ Esercizio - 16/exercise-16.js
@@ -0,0 +1,26 @@
+
+
+function createStore() {
+  
+  const product = [];
+
+  return function newProduct(param){
+    product.push(param);
+    console.log(product);
+  }
+
+};
+
+
+const redPants = { id: 1, name: 'Red Pants' };
+const whiteHat = { id: 2, name: 'White Hat' };
+const blackSneakers = { id: 3, name: 'Black Sneakers' };
+
+console.log('--- Dress Store ---');
+const dressStore = createStore();
+dressStore(redPants);
+dressStore(whiteHat);
+
+console.log('--- Shoes Store ---');
+const shoesStore = createStore();
+shoesStore(blackSneakers);
