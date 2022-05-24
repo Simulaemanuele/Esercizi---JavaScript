5afc361f4ee538fa76ef253c067b20c5fbbb8069
Emanuele Simula <simulaemanuele@gmail.com>
2022-05-24 14:41:09 +0200

    exercise: ex. 30 done

---
+++ Esercizio - 30/README.md
@@ -0,0 +1,7 @@
+# Array Methods - Exercise 2
+Implementare la funzione `nicknameMap` che, dato un array di persone, crea un array di nickname. Il nickname deve essere formato in questa modo: `<name>-<age>`.
+
+Esempio:
+```
+{ name: 'Paul', age: 21 } => Paul-21
+```
---
+++ Esercizio - 30/exercise-30.js
@@ -0,0 +1,26 @@
+function nicknameMap(persons) {
+  const nickMapped = persons.map(function(nick){
+    return {
+      nickname: `${nick.name}-${nick.age}`
+    };
+  });
+
+  return nickMapped;
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const nicknames = nicknameMap(persons);
+console.log(persons);
+console.log(nicknames);
