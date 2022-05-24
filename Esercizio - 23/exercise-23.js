656c6a0674e9e87b35c73360f90da1bc5d06c2b8
Emanuele Simula <simulaemanuele@gmail.com>
2022-05-24 12:22:07 +0200

    exercise: ex. 23 done

---
+++ Esercizio - 23/exercise-23.js
@@ -0,0 +1,11 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+// Print values of person using Object.keys
+console.log('The Object keys are: \t')
+console.log(Object.keys(person));
+console.log('The Object values are: \t');
+console.log(Object.values(person));
---
+++ Esercizio - 23/exercise-23.md
@@ -0,0 +1,8 @@
+# Object Methods - Exercise 1
+Utilizzando l'oggetto `person` stampare in console i suoi valori nel seguente modo utilizzando il metodo `Object.keys`:
+
+```
+firstName: Mario
+lastName: Rossi
+age: 25
+```
