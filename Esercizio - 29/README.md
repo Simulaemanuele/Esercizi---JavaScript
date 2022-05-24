93e43af3504045c40e39b73bbb37f5fd68fba8d2
Emanuele Simula <simulaemanuele@gmail.com>
2022-05-24 14:30:54 +0200

    exercise: ex. 29 done

---
+++ Esercizio - 29/README.md
@@ -0,0 +1,2 @@
+# Array Methods - Exercise 1
+Implementare la funzione `adultFilter` che, dato un array di persone, filtra e restituisce soltanto coloro che sono maggiorenni.
---
+++ Esercizio - 29/exercise-29.js
@@ -0,0 +1,25 @@
+function adultFilter(persons) {
+  
+  const adults = persons.filter(function(persons){
+    return persons.age >= 18;
+  });
+
+  return adults;  
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const adults = adultFilter(persons);
+console.log(persons);
+console.log(adults);
