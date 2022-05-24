7d9e8f8c6c091b0438baa4ad7dc8a95f1be3d79b
Emanuele Simula <simulaemanuele@gmail.com>
2022-05-24 12:42:33 +0200

    exercise: ex. 24 done

---
+++ Esercizio - 24/exercise-24.js
@@ -0,0 +1,9 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+// Print values of person using Object.values
+console.log('The Object values are: \t');
+console.log(Object.values(person));
---
+++ Esercizio - 24/exercise-24.md
@@ -0,0 +1,8 @@
+# Object Methods - Exercise 2
+Utilizzando l'oggetto `person` stampare in console i suoi valori nel seguente modo utilizzando il metodo `Object.values`:
+
+```
+Mario
+Rossi
+25
+```
