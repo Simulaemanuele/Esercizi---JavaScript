4db3cd4a88454df72679b082203d56ee47ee096b
Emanuele Simula <simulaemanuele@gmail.com>
2022-06-02 09:50:44 +0200

    exercise: ex. 38 done

---
+++ Esercizio - 38/README.md
@@ -0,0 +1,3 @@
+# Classes - Exercise 5
+
+Definire la classe `BankAccountVip` che estendendo dalla classe `BankAccount`, oltre a consentire tutte le operazioni di quest'ultima, permette all'utente di ottenere un interesse del 3% sull'importo depositato, ma solo se quest'ultimo è maggiore o uguale a 1000€. Il risultato finale deve essere 5541.
---
+++ Esercizio - 38/exercise-38.js
@@ -0,0 +1,39 @@
+class BankAccount {
+  #amount = 0;
+
+  constructor(initialAmount) {
+    this.#amount = initialAmount;
+  }
+
+  deposit(amount) {
+    this.#amount += amount;
+  }
+
+  withdraw(amount) {
+    this.#amount -= amount;
+  }
+
+  view() {
+    console.log(this.#amount);
+  }
+}
+
+class BankAccountVip extends BankAccount {
+  
+constructor(initialAmount){
+  super(initialAmount)
+    
+  }
+
+
+    deposit = amount => amount >= 1000 ? super.deposit(amount + ((amount/100) *3)) : super.deposit(amount);
+  
+}
+
+
+const bankAccountVip = new BankAccountVip(1000);
+bankAccountVip.deposit(500);
+bankAccountVip.deposit(1200);
+bankAccountVip.withdraw(800);
+bankAccountVip.deposit(3500);
+bankAccountVip.view();
