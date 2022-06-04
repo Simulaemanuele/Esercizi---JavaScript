3dae59b10dc287e281ec91d58c967dd670f535cd
Emanuele Simula <simulaemanuele@gmail.com>
2022-06-04 13:01:06 +0200

    exercise: ex. 47 done

---
+++ Esercizio - 47/README.md
@@ -0,0 +1,9 @@
+# Destructuring assignment - Exercise 4
+Utilizzare la destrutturazione per stampare la lista dei valori delle proprietà presenti in `person`:
+
+```
+id: 1
+firstName: Mario
+lastName: Rossi
+age: 25
+```
---
+++ Esercizio - 47/exercise-47.js
@@ -0,0 +1,12 @@
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+};
+
+
+const {id, firstName, lastName, age} = person;
+
+console.log(` '''\n id: ${id},\n firstName: ${firstName},\n lastName: ${lastName},\n age: ${age}\n\n '''`);
+
