738e48a576790061a27487ab2f94b4693b6dcff6
Emanuele Simula <simulaemanuele@gmail.com>
2022-06-06 15:44:48 +0200

    fix: ex. 27 fixed

--- Esercizio - 27/exercise-27.js
+++ Esercizio - 27/exercise-27.js
@@ -6,17 +6,46 @@ const person = {
 console.log('Oggetto person: \t')
 console.log(person);
 
-//console.log(person.address.city);  Argh! Cannot read property 'city' of undefined
-//console.log(person.fullName());  Argh! Cannot read property 'fullName()' of undefined
+// Using .? operator in console.log() I can bypass the error
 
-if(person.address) {
-  console.log(person.address.city);
-} else {
-  console.log('La proprietà "city" non esiste!\n')
-};
+    console.log(person?.address?.city);  //Argh! Cannot read property 'city' of undefined
+    console.log(person?.fullName?.());  //Argh! Cannot read property 'fullName()' of undefined
 
-if(person.fullName){
-  console.log(person.fullName());
-} else {
-  console.log('La funzione "fullName()" non esiste! \n')
-}
+// Here's the same but in a ternary operator that it permitt to valutate that the two properties inexistence
+
+    person?.address?.city ? console.log(person.address.city) : console.log('La proprietà city non esiste!\n');
+
+    person?.fullName?.() ? console.log(person.fullName()) : console.log('La funzione "fullName()" non esiste! \n');
+
+
+// Previous wrong version to review
+
+    /*if(person.address) {
+      console.log(person.address.city);
+    } else {
+      console.log('La proprietà "city" non esiste!\n')
+    };
+
+    if(person.fullName){
+      console.log(person.fullName());
+    } else {
+      console.log('La funzione "fullName()" non esiste! \n')
+    }*/
+
+// .? operator correction done
+
+    /*if(person?.address?.city){
+      console.log(person.address.city);
+    } else {
+      console.log('La proprietà "city" non esiste!\n')
+    };*/
+
+
+
+    /*if(person?.fullName?.()){
+      console.log(person.fullName());
+    } else {
+      console.log('La funzione "fullName()" non esiste! \n')
+    };*/
+
+
