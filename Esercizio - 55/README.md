9625769945cb70e8c9f1642c00939af9f4e44523
Emanuele Simula <simulaemanuele@gmail.com>
2022-06-11 13:38:58 +0200

    exercise: ex. 55 done

---
+++ Esercizio - 55/README.md
@@ -0,0 +1,2 @@
+# Rest parameters and spread syntax - Exercise 3
+Come mergiare il valore di `newNumber` all'interno di `numberStore` senza utilizzare il metodo `push`?
---
+++ Esercizio - 55/exercise-55.js
@@ -0,0 +1,6 @@
+let numberStore = [0, 1, 2];
+let newNumber = 3;
+
+numberStore = [...numberStore, newNumber];
+//numberStore.push(newNumber);
+console.log(numberStore);
